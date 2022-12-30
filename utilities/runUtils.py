a0f4fdad0822f6404469a5ed1f40dc253866cf53
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2022-12-30 16:50:31 +0400

    [add] Support Class for dict objects to be accessed using (.) like objects

--- utilities/runUtils.py
+++ utilities/runUtils.py
@@ -38,3 +38,37 @@ def freeze_params(model, exclusion_list = []):
     for param in model.parameters():
         param.requires_grad = False
     return model
+
+
+
+
+class ObjDict(dict):
+    """
+    reference: https://stackoverflow.com/a/32107024
+    """
+    def __init__(self, *args, **kwargs):
+        super(ObjDict, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    self[k] = v
+        if kwargs:
+            for k, v in kwargs.items():
+                self[k] = v
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(ObjDict, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(ObjDict, self).__delitem__(key)
+        del self.__dict__[key]
