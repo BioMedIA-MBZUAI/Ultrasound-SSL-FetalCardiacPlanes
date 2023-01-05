b498a1de5af2892cb759efb4248c5d97ee317bd3
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2023-01-05 17:06:05 +0400

    [model] add code loading capacity from pretrained SSL

--- algorithms/resnet.py
+++ algorithms/resnet.py
@@ -1,7 +1,9 @@
+import os
 import torch
 import torchvision
 from torch import nn
 import utilities.runUtils as rutl
+import utilities.logUtils as lutl
 
 
 ## Neural Net
@@ -13,11 +15,11 @@ class ClassifierNet(nn.Module):
         self.args = args
 
         # Feature Extractor
-        self.backbone = self._load_resnet_backbone()
+        self.backbone, out_size = self._load_resnet_backbone()
         self.feat_dropout = nn.Dropout(p=self.args.featx_dropout)
 
         # Classifier
-        sizes = [2048] + list(args.classifier)
+        sizes = [out_size] + list(args.classifier)
         layers = []
         for i in range(len(sizes) - 2):
             layers.append(nn.Linear(sizes[i], sizes[i + 1], bias=False))
@@ -41,13 +43,15 @@ class ClassifierNet(nn.Module):
 
         torch_pretrain = "DEFAULT" if self.args.featx_pretrain == "DEFAULT" else None
 
-
         if self.args.feature_extract == 'resnet18':
-            backbone = torchvision.models.resnet50(zero_init_residual=True,
+            backbone = torchvision.models.resnet18(zero_init_residual=True,
                                  weights=torch_pretrain)
+            outfeat_size = 2048
         elif self.args.feature_extract == 'resnet50':
             backbone = torchvision.models.resnet50(zero_init_residual=True,
                                 weights=torch_pretrain)
+            outfeat_size = 512
+        backbone.fc = nn.Identity() #remove fc of default arch
 
         # Change input Conv
         # conv1_weight = torch.sum(backbone.conv1.weight, axis = 1).unsqueeze(1)
@@ -55,8 +59,11 @@ class ClassifierNet(nn.Module):
         # with torch.no_grad():
         #     backbone.conv1.weight.copy_(conv1_weight)
 
-
-        backbone.fc = nn.Identity() #remove fc of default arch
+        # pretrain from external file
+        if os.path.exists(self.args.featx_pretrain):
+            backbone = self._load_weights_from_file(backbone,
+                                self.args.featx_pretrain )
+            print("Loaded:", self.args.featx_pretrain )
 
         # freeze model
         if self.args.featx_freeze:
@@ -64,4 +71,31 @@ class ClassifierNet(nn.Module):
             for param in backbone.parameters():
                 param.requires_grad = False
 
-        return backbone
+        return backbone, outfeat_size
+
+
+
+    def _load_weights_from_file(self, model, weight_path, flexible = True):
+        def _purge(key): # hardcoded logic
+            return key.replace("backbone.", "")
+
+        model_dict = model.state_dict()
+        weight_dict = torch.load(weight_path)
+
+        if 'model' in weight_dict.keys(): pretrain_dict = weight_dict['model']
+        else:   pretrain_dict = weight_dict
+
+        pretrain_dict = { _purge(k) : v for k, v in pretrain_dict.items()}
+
+        if flexible:
+            pretrain_dict = {k: v for k, v in pretrain_dict.items() if k in model_dict}
+        if not len(pretrain_dict):
+            raise Exception(f"No weight names match to be loaded; though file exits ! {weight_path}, Dict: {weight_dict.keys()}")
+
+        lutl.LOG2TXT(f"Pretrained layers:{pretrain_dict.keys()}" ,
+                        self.args.gLogPath+"/misc.txt", console=False )
+
+        model_dict.update(pretrain_dict)
+        model.load_state_dict(model_dict)
+
+        return model
