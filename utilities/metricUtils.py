d5c35a2528b4d862a69ca9268fe858276333e94c
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2023-01-05 11:48:56 +0400

    metrics update with better logging

--- utilities/metricUtils.py
+++ utilities/metricUtils.py
@@ -1,18 +1,22 @@
+import os, csv
 import numpy as np
 import torch
 from sklearn import metrics as skmetrics
 
 class MultiClassMetrics():
-    def __init__(self):
+    def __init__(self, logpath):
         self.tgt = []
         self.prd = []
         self.nnloss = []
-    def reset(self):
-        self.__init__()
+        self.logpath = logpath
+
+    def reset(self, save_results = False):
+        if save_results: self._write_csv()
+        self.__init__(self.logpath)
 
     def add_entry(self, prd, tgt, loss=0):
         self.prd.extend(prd.cpu().detach().numpy())
-        self.tgt.extend(tgt.cpu())
+        self.tgt.extend(tgt.cpu().detach().numpy())
         if loss: self.nnloss.append(loss.cpu().detach().numpy())
 
     def get_loss(self):
@@ -27,6 +31,13 @@ class MultiClassMetrics():
 
     def get_stat_summary(self):
         pass
+
+
+    def _write_csv(self):
+        with open(os.path.join(self.logpath, "predict.csv"), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(["target", "prediction"])
+            writer.writerows(zip(self.tgt, self.prd))
 
 
 if __name__ == "__main__":
