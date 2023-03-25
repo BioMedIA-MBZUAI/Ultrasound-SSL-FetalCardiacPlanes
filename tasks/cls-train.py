f55860977e93edad4f1b3401a5d6955b878cb9d1
Joseph Geo Benjamin <josephgeobenjamin@gmail.com>
2023-03-26 01:58:44 +0400

    Upgraded training file with simpler routines

--- tasks/cls-train.py
+++ tasks/cls-train.py
@@ -3,27 +3,24 @@
 
 import argparse
 import json
-import math
 import os
-import random
-import signal
-import subprocess
 import sys
 import time
-
 from tqdm.autonotebook import tqdm
-import PIL.Image
-from torch import nn, optim
+
 import torch
+from torch import nn, optim
+import torchinfo
+
+import numpy as np
+from sklearn.model_selection import train_test_split as sk_train_test_split
 
 sys.path.append(os.getcwd())
 import utilities.runUtils as rutl
 import utilities.logUtils as lutl
 from utilities.metricUtils import MultiClassMetrics
-from algorithms.convnext import ClassifierNet
-# from algorithms.classifiers import ClassifierNet
-from datacode.ultrasound_data import getUSClassifyDataloader
-from datacode.general_data import getCifar100Dataloader
+from algorithms.classifiers import ClassifierNet
+from datacode.ultrasound_data import ClassifyDataFromCSV, get_class_weights
 
 
 print(f"Pytorch version: {torch.__version__}")
@@ -33,112 +30,170 @@ print("Device Used:", device)
 
 ###============================= Configure and Setup ===========================
 
-cfg = rutl.ObjDict(
-train_images_folder= "/home/USR/WERK/data/train",
-train_csv_file = None,
-train_filtering = {},  #{"blacklist":{'class':["4ch"],"machine_type":["Voluson E8","Voluson S10 Expert","V830"]}}
-balance_class = True,
-augument_list = ["hflp", "vflp"], #["crop", "ctrs", "brig", "affn", "pers", "hflp", "vflp"]
+CFG = rutl.ObjDict(
 
-valid_images_folder = "/home/USR/WERK/data/valid",
-valid_csv_file = None,
-valid_filtering = {},
+data_folder  = "/home/joseph.benjamin/WERK/data/Fetal-UltraSound/US-Planes-Heart-Views-V3/",
+balance_data = False, #while loading in dataloader; removed
 
+epochs     = 100,
+image_size = 256,
+batch_size = 64,
+workers    = 12,
+learning_rate = 1e-3,
+weight_decay  = 1e-6,
 
-epochs= 1000,
-batch_size= 2048,
-workers= 4,
-learning_rate= 0.2,
-weight_decay= 1e-6,
-feature_extract = "resnet50", # "resnet34/50/101"
-featx_pretrain =  "DEFAULT",  # path-to-weights or None
-featx_dropout = 0,
-featx_freeze =  False,
+featx_arch     = "resnet50",
+featx_pretrain =  "IMGNET-1K" , # "IMGNET-1K" or None
+featx_dropout = 0.5,
+clsfy_layers = [5], #First mlp inwill be set w.r.t FeatureExtractor
+clsfy_dropout = 0.0,
 
-classifier = [512, 6],
-clsfy_dropout = 0.5,
-
-checkpoint_dir= "hypotheses/dummypth/",
-restart_training=False
+checkpoint_dir= "hypotheses/#dummy/Classify/trail-002",
+restart_training=True
 )
 
 ### -----
 parser = argparse.ArgumentParser(description='Classification task')
-parser.add_argument('--load_json', default='configs/cls-train-cfg.json', type=str, metavar='JSON',
+parser.add_argument('--load_json', type=str, metavar='JSON',
     help='Load settings from file in json format. Command line options override values in file.')
 
 args = parser.parse_args()
 
 if args.load_json:
     with open(args.load_json, 'rt') as f:
-        cfg.__dict__.update(json.load(f))
+        CFG.__dict__.update(json.load(f))
 
 ### ----------------------------------------------------------------------------
-cfg.gLogPath = cfg.checkpoint_dir
-cfg.gWeightPath = cfg.checkpoint_dir + '/weights/'
+CFG.gLogPath = CFG.checkpoint_dir
+CFG.gWeightPath = CFG.checkpoint_dir + '/weights/'
 
 ### ============================================================================
 
+def getDataLoaders(data_percent=None):
+    ## Augumentations
 
-def simple_main():
+    traindataset = ClassifyDataFromCSV(CFG.data_folder,
+                                       CFG.data_folder+"/trainV3.csv",
+                                       transform = None,)
+    validdataset = ClassifyDataFromCSV(CFG.data_folder,
+                                       CFG.data_folder+"/validV3.csv",
+                                       transform = None,)
+    class_weights, _ = get_class_weights(traindataset.targets, nclasses=5)
 
-    ### SETUP
+    ## Chose P% of data from train data
+    if data_percent and (data_percent < 100):
+        _idx, used_idx = sk_train_test_split( np.arange(len(traindataset)),
+                                test_size=data_percent/100, random_state=1729,
+                                stratify=traindataset.targets)
+        traindataset = torch.utils.data.Subset(traindataset, sorted(used_idx))
+        lutl.LOG2CSV(sorted(used_idx), CFG.gLogPath +'/train_indices_used.csv')
+
+
+    ## Loaders
+    trainloader  = torch.utils.data.DataLoader( traindataset, shuffle=True,
+                        batch_size=CFG.batch_size, num_workers=CFG.workers,
+                        pin_memory=True)
+
+    validloader  = torch.utils.data.DataLoader( validdataset, shuffle=False,
+                        batch_size=CFG.batch_size, num_workers=CFG.workers,
+                        pin_memory=True)
+
+    lutl.LOG2DICTXT({"Train-":len(traindataset),
+                     "class-weights":str(class_weights)},
+                     CFG.gLogPath +'/misc.txt')
+    lutl.LOG2DICTXT({"Valid-":len(validdataset)}, CFG.gLogPath +'/misc.txt')
+
+    return trainloader, validloader, class_weights
+
+
+def getModelnOptimizer():
+
+    ## pretrain setting
+    m_state = 0; torch_pretrain_flag = None
+    if os.path.isfile(CFG.featx_pretrain):
+        m_state = torch.load(CFG.featx_pretrain, map_location='cpu')
+    else: torch_pretrain_flag = CFG.featx_pretrain
+
+    model = ClassifierNet(arch=CFG.featx_arch,
+                    fc_layer_sizes=CFG.clsfy_layers,
+                    feature_dropout=CFG.featx_dropout,
+                    classifier_dropout=CFG.clsfy_dropout,
+                    torch_pretrain=torch_pretrain_flag )
+
+    if m_state:
+        ret_msg = model.backbone.load_state_dict(m_state, strict=False)
+        lutl.LOG2TXT(f"Manual Pretrain Loaded...{CFG.featx_pretrain},{str(ret_msg)}",
+                     CFG.gLogPath +'/misc.txt')
+
+    model_info = torchinfo.summary(model, (1, 3, CFG.image_size, CFG.image_size),
+                                verbose=0)
+    lutl.LOG2TXT(model_info, CFG.gLogPath +'/misc.txt', console= False)
+
+    ##--------------
+
+    optimizer = optim.AdamW(model.parameters(), lr=CFG.learning_rate,
+                        weight_decay=CFG.weight_decay)
+
+    scheduler = False
+
+    return model.to(device), optimizer, scheduler
+
+
+def getLossFunc(class_weights):
+    lossfn = nn.CrossEntropyLoss(weight=torch.tensor(class_weights,
+                                        dtype=torch.float32).to(device) )
+    return lossfn
+
+
+
+def simple_main(data_percent=None):
+
+   ### SETUP
     rutl.START_SEED()
     gpu = 0
     torch.cuda.set_device(gpu)
     torch.backends.cudnn.benchmark = True
 
-    if os.path.exists(cfg.checkpoint_dir) and (not cfg.restart_training):
-        raise Exception("CheckPoint folder already exists and restart_training not enabled; Somethings Wrong!")
-    if not os.path.exists(cfg.gWeightPath): os.makedirs(cfg.gWeightPath)
+    ## paths and logs setup
+    if data_percent: CFG.gLogPath = CFG.checkpoint_dir+f"/{data_percent}_percent/"
+    CFG.gWeightPath = CFG.gLogPath+"/weights/"
 
-    with open(cfg.gLogPath+"/exp_cfg.json", 'a') as f:
-        json.dump(vars(cfg), f, indent=4)
+    if os.path.exists(CFG.gLogPath) and (not CFG.restart_training):
+        raise Exception("CheckPoint folder already exists and restart_training not enabled; Somethings Wrong!",
+                        CFG.checkpoint_dir)
+    if not os.path.exists(CFG.gWeightPath): os.makedirs(CFG.gWeightPath)
 
-
-    ### MODEL, OPTIM
-    model = ClassifierNet(cfg).cuda(gpu)
-    lossfn = nn.CrossEntropyLoss()
-    optimizer = optim.AdamW(model.parameters(), lr=cfg.learning_rate,
-                        weight_decay=cfg.weight_decay)
-    lutl.LOG2TXT(f"Parameters:{rutl.count_train_param(model)}", cfg.gLogPath +'/misc.txt')
-
-    ## Automatically resume from checkpoint if it exists and enabled
-    if os.path.exists(cfg.gWeightPath +'/checkpoint.pth'):
-        ckpt = torch.load(cfg.gWeightPath+'/checkpoint.pth',
-                          map_location='cpu')
-        start_epoch = ckpt['epoch']
-        model.load_state_dict(ckpt['model'])
-        optimizer.load_state_dict(ckpt['optimizer'])
-        lutl.LOG2TXT(f"Restarting Training from EPOCH:{start_epoch} of {cfg.checkpoint_dir}",  cfg.gLogPath +'/misc.txt')
-    else:
-        start_epoch = 0
+    with open(CFG.gLogPath+"/exp_cfg.json", 'a') as f:
+        json.dump(vars(CFG), f, indent=4)
 
 
     ### DATA ACCESS
-    trainloader, data_info = getUSClassifyDataloader(cfg.train_images_folder,
-                        csv_file = cfg.train_csv_file,
-                        batch_size=cfg.batch_size, workers=cfg.workers,
-                        type_='train',
-                        augument_list=cfg.augument_list,
-                        balance_class=cfg.balance_class,
-                        filtering_dict=cfg.train_filtering )
-    lutl.LOG2DICTXT(data_info, cfg.gLogPath +'/misc.txt')
-    validloader, data_info = getUSClassifyDataloader(cfg.valid_images_folder,
-                        csv_file = cfg.valid_csv_file,
-                        batch_size=cfg.batch_size, workers=cfg.workers,
-                        type_='valid',
-                        filtering_dict=cfg.valid_filtering)
-    lutl.LOG2DICTXT(data_info, cfg.gLogPath +'/misc.txt')
+    trainloader, validloader, class_weights  = getDataLoaders(data_percent)
 
+    ### MODEL, OPTIM
+    model, optimizer, scheduler = getModelnOptimizer()
+    lossfn = getLossFunc(class_weights)
+
+
+    ## Automatically resume from checkpoint if it exists and enabled
+    if os.path.exists(CFG.gWeightPath +'/checkpoint.pth') and CFG.restart_training:
+        ckpt = torch.load(CFG.gWeightPath  +'/checkpoint.pth',
+                            map_location='cpu')
+        start_epoch = ckpt['epoch']
+        model.load_state_dict(ckpt['model'])
+        optimizer.load_state_dict(ckpt['optimizer'])
+        lutl.LOG2TXT(f"Restarting Training from EPOCH:{start_epoch} of {CFG.gLogPath}",  CFG.gLogPath +'/misc.txt')
+    else:
+        start_epoch = 0
 
     ### MODEL TRAINING
     start_time = time.time()
     best_acc = 0 ; best_loss = float('inf')
-    trainMetric = MultiClassMetrics(cfg.gLogPath)
-    validMetric = MultiClassMetrics(cfg.gLogPath)
-    scaler = torch.cuda.amp.GradScaler() # for mixed precision
-    for epoch in range(start_epoch, cfg.epochs):
+    trainMetric = MultiClassMetrics(CFG.gLogPath)
+    validMetric = MultiClassMetrics(CFG.gLogPath)
+
+    # scaler = torch.cuda.amp.GradScaler() # for mixed precision
+    for epoch in range(start_epoch, CFG.epochs):
 
         ## ---- Training Routine ----
         model.train()
@@ -146,22 +201,19 @@ def simple_main():
             img = img.to(device, non_blocking=True)
             tgt = tgt.to(device, non_blocking=True)
             optimizer.zero_grad()
-            ## with mixed precision
-            with torch.cuda.amp.autocast():
-                pred = model.forward(img)
-                loss = lossfn(pred, tgt)
-            ## END with
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
-            scaler.update()
-            # loss.backward()
-            # optimizer.step()
+            pred = model.forward(img)
+            loss = lossfn(pred, tgt)
+            loss.backward()
+            # nn.utils.clip_grad_norm_(model.parameters(),
+            #                          max_norm=2.0, norm_type=2)
+            optimizer.step()
             trainMetric.add_entry(torch.argmax(pred, dim=1), tgt, loss)
+        if scheduler: scheduler.step()
 
         ## save checkpoint states
         state = dict(epoch=epoch + 1, model=model.state_dict(),
                         optimizer=optimizer.state_dict())
-        torch.save(state, cfg.gWeightPath +'/checkpoint.pth')
+        torch.save(state, CFG.gWeightPath +'/checkpoint.pth')
 
 
         ## ---- Validation Routine ----
@@ -170,37 +222,48 @@ def simple_main():
             for img, tgt in tqdm(validloader):
                 img = img.to(device, non_blocking=True)
                 tgt = tgt.to(device, non_blocking=True)
-                # with torch.cuda.amp.autocast():
                 pred = model.forward(img)
                 loss = lossfn(pred, tgt)
-                ## END with
                 validMetric.add_entry(torch.argmax(pred, dim=1), tgt, loss)
 
-        ## Log Metrics
-        stats = dict( epoch=epoch, time=int(time.time() - start_time),
-                    trainloss = trainMetric.get_loss(),
-                    trainacc = trainMetric.get_accuracy(),
-                    validloss = validMetric.get_loss(),
-                    validacc = validMetric.get_accuracy(), )
-        lutl.LOG2DICTXT(stats, cfg.gLogPath+'/train-stats.txt')
+        ## Log Metrics TODO Add balanced and F1
+        stats = dict(
+                epoch=epoch, time=int(time.time() - start_time),
+                trainloss = trainMetric.get_loss(),
+                trainacc  = trainMetric.get_balanced_accuracy(),
+                trainF1   = trainMetric.get_f1score(),
+                validloss = validMetric.get_loss(),
+                validacc  = validMetric.get_balanced_accuracy(),
+                validF1   = validMetric.get_f1score(),
+                )
+        lutl.LOG2DICTXT(stats, CFG.gLogPath+'/train-stats.txt')
 
 
         ## save best model
         best_flag = False
         if stats['validacc'] > best_acc:
-            torch.save(model.state_dict(), cfg.gWeightPath +'/bestmodel.pth')
+            torch.save(model.state_dict(), CFG.gWeightPath +'/bestmodel.pth')
             best_acc = stats['validacc']
             best_loss = stats['validloss']
             best_flag = True
 
         ## Log detailed validation
-        detail_stat = dict( epoch=epoch, time=int(time.time() - start_time),
-                            best = best_flag,
-                            validreport =  validMetric.get_class_report() )
-        lutl.LOG2DICTXT(detail_stat, cfg.gLogPath+'/validation-details.txt', console=False)
+        detail_stat = dict(
+                epoch=epoch, time=int(time.time() - start_time),
+                best = best_flag,
+                validf1scr  = validMetric.get_f1score(),
+                validbalacc = validMetric.get_balanced_accuracy(),
+                validacc    = validMetric.get_accuracy(),
+                validreport = validMetric.get_class_report(),
+                validconfus = validMetric.get_confusion_matrix().tolist(),
+            )
+        lutl.LOG2DICTXT(detail_stat, CFG.gLogPath+'/validation-details.txt', console=False)
 
         trainMetric.reset()
         validMetric.reset(best_flag)
 
 if __name__ == '__main__':
     simple_main()
+
+    # for p in [100, 50, 25, 10, 5, 1]:
+    #     simple_main(data_percent=p)
